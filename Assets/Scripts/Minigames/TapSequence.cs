588a03eaa1836d6cefde36b5552daffc21e545ac
Andy Brenneke <sneakymax@gmail.com>
2016-02-14 06:57:59 -0800

    Tap Sequence

---
+++ Assets/Scripts/Minigames/TapSequence.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TapSequence : MinigameScriptBase
+    {
+        public TapSequenceItem[] Items;
+
+        private int currentIndex;
+        private TapSequenceItem currentItem;
+
+        protected override void OnUnityStart()
+        {
+            foreach (var item in Items)
+            {
+                var tapItem = item.GetComponent<TapSequenceItem>();
+                tapItem.Tapped += () => ItemTapped(tapItem);
+
+                item.gameObject.SetActive(false);
+            }
+        }
+
+        private void ItemTapped(TapSequenceItem item)
+        {
+            if (item != currentItem)
+                return;
+
+            SetNextItem();
+        }
+
+        protected override void OnStartMinigame()
+        {
+            currentIndex = -1;
+            SetNextItem();
+        }
+
+        private void SetNextItem()
+        {
+            if (currentItem != null)
+                currentItem.gameObject.SetActive(false);
+
+            currentIndex++;
+
+            if (currentIndex >= Items.Length)
+            {
+                currentItem = null;
+                MarkAsSuccess();
+                return;
+            }
+
+            currentItem = Items[currentIndex];
+            currentItem.gameObject.SetActive(true);
+        }
+
+        protected override void OnUnityUpdate()
+        {
+            if (currentItem == null)
+                return;
+
+            if (currentItem.isActiveAndEnabled == false)
+                currentItem.gameObject.SetActive(true);
+        }
+
+        protected override void OnTimeElapsed()
+        {
+        }
+
+        protected override void CancelAnyCoroutines()
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/TapSequenceItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TouchScript.Gestures;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TapSequenceItem : MonoBehaviour
+    {
+        public PressGesture PressGesture;
+
+        public event Action Tapped;
+
+        public void Start()
+        {
+            PressGesture.Pressed += PressGestureOnPressed;            
+        }
+
+        private void PressGestureOnPressed(object sender, EventArgs eventArgs)
+        {
+            if (Tapped != null)
+                Tapped();
+        }
+    }
+}
