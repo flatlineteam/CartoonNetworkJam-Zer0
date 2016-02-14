7c7bb3361cb240e940a85de0d8bbd89502d9ec02
Andy Brenneke <sneakymax@gmail.com>
2016-02-13 22:11:04 -0800

    PlaySoundOnEnabled

---
+++ Assets/Scripts/PlaySoundOnEnabled.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlaySoundOnEnabled : MonoBehaviour
+    {
+        public AudioClip AudioClip;
+
+        public void OnEnabled()
+        {
+            SoundKit.instance.playOneShot(AudioClip);
+        }
+    }
+}
