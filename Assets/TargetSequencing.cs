1ac59fad5daa1bb8a970884453b85596b54f46cc
Rojibora <Rojibora@gmail.com>
2016-02-13 23:09:11 -0800

    Hair dry modification

---
+++ Assets/TargetSequencing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TouchScript.Gestures;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TargetSequencing : MonoBehaviour 
+    {
+        public Collider2D[] itemSquence;
+
+
+    	
+    }
+}
