8a7fb9c2cab5c77eaa7343bf54beb32573510807
Andy Brenneke <sneakymax@gmail.com>
2016-02-14 12:46:36 -0800

    Burrito & aspect ratio fix

---
+++ Assets/Scripts/CameraInit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CameraInit : MonoBehaviour
+    {
+        public void Awake()
+        {
+            StartCoroutine(Refresh());
+        }
+
+        public IEnumerator Refresh()
+        {
+            while (true)
+            {
+                var aspectRatio = 4.0f / 3.0f;
+
+                var screenAspectRatio = (float)Screen.width / Screen.height;
+
+                var selfCamera = GetComponent<Camera>();
+
+                var scaleHeight = screenAspectRatio / aspectRatio;
+                Debug.Log(scaleHeight);
+
+                if (scaleHeight > 1.0f)
+                {
+                    selfCamera.orthographicSize = 5 / scaleHeight;
+                }
+
+                yield return new WaitForSeconds(1);
+            }
+        }
+    }
+}
