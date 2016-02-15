ac6a0485249c3525eb1108bdd2f245cd77602af3
Andy Brenneke <sneakymax@gmail.com>
2016-02-14 19:34:32 -0800

    Credits

--- Assets/Scripts/CreditsController.cs
+++ Assets/Scripts/CreditsController.cs
@@ -5,11 +5,16 @@ namespace Assets.Scripts
 {
     public class CreditsController : MonoBehaviour
     {
+        public void Back()
+        {
+            SceneManager.LoadScene(k.Scenes.TITLE);
+        }
+
         public void Update()
         {
             if (Input.GetKeyDown(KeyCode.Escape))
             {
-                SceneManager.LoadScene(k.Scenes.TITLE);
+                Back();
             }                
         }
     }
