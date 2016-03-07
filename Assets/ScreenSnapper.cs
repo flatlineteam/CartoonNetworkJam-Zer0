3ad09362610a0b225a6c351f49910d36e705d366
Rojibora <Rojibora@gmail.com>
2016-03-06 16:15:45 -0800

    Swapped thumb with new thumb
    updated multi tap minigame with new assets and minor behavior changes.

---
+++ Assets/ScreenSnapper.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenSnapper : MonoBehaviour {
+
+    public bool xNeg = false;
+    public bool xPos = false;
+
+    public bool yNeg = false;
+    public bool yPos = false;
+
+    private float HalfScreenWidth;
+    private float HalfScreenHeight;
+
+	// Use this for initialization
+	void Start () {
+        HalfScreenWidth = Camera.main.orthographicSize * Screen.width / Screen.height;
+        HalfScreenHeight = HalfScreenWidth * ((float)Screen.height / Screen.width);
+
+        var horzSnap = 0;
+        var vertSnap = 0;
+
+        if(xNeg) horzSnap = -1;
+        if(xPos) horzSnap = 1;
+
+        if(yNeg) vertSnap = -1;
+        if(yPos) vertSnap = 1;
+
+        var newPosition = transform.position;
+
+        if(horzSnap != 0)
+            newPosition.x = horzSnap * HalfScreenWidth;
+        if(vertSnap != 0)
+            newPosition.y = vertSnap * HalfScreenHeight;
+
+        transform.position = newPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
--- Assets/Scripts/ImageSequencer.cs
+++ Assets/Scripts/ImageSequencer.cs
@@ -5,28 +5,26 @@ namespace Assets.Scripts
 {
     public class ImageSequencer : MonoBehaviour 
     {
-        public Sprite[] backgroundImages;
+        public GameObject[] backgroundImages;
 
-        private int CurImageIndex = 0;
+        public AudioClip nailSound;
 
-        private SpriteRenderer SpriteRend;
-
-        void Start()
-        {
-            SpriteRend = GetComponent<SpriteRenderer>();
-            SpriteRend.sprite = backgroundImages[0];
-        }
-
-        void Update()
-        {
-            SpriteRend.sprite = backgroundImages[CurImageIndex];
-        }
+        private int CurImageIndex = -1;              
 
         public void incrementIndex()
         {
             CurImageIndex++;
             if (CurImageIndex >= backgroundImages.Length)
                 CurImageIndex = backgroundImages.Length;
+            else
+            {
+                backgroundImages[CurImageIndex].SetActive(true);
+
+                //SoundKit.instance.playOneShot(nailSound, 1f);
+                SoundKit.instance.playPitchedSound(nailSound, 0.4f);
+
+                Camera.main.GetComponent<ScreenShake>().ShakeCamera(0.2f, System.TimeSpan.FromSeconds(0.2));
+            }
         }
     }
 }
